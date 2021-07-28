cc24bb8ca4f97fc87d69cc28925121917ed6e0f0
Simon Capriles <simon.capriles@kyohei.com.bo>
2021-07-27 21:17:11 -0400

    Improved apps

--- eyes.js
+++ eyes.js
@@ -1,22 +1,18 @@
 const balls = document.getElementsByClassName('ball');
 var eyes = document.getElementsByClassName('eyes');
 var eye_list = []
-var eye_number = 3;
+var eye_number = 0;
 
-for (let i=0;i<eye_number;i++){
+const addOne = ()=>{
+    eye_number += 1;
     var div = document.createElement('div');
     let eye = eyes[0].appendChild(div);
     eye.classList.add('eye');
-    eye.setAttribute('id',`eye${i}`);
-    eye_list.push(eye)
+    eye.setAttribute('id',`eye${eye_number-1}`);
+    eye_list.push(eye);
+    console.log(eye_list)
+    eye_list[eye_number - 1].appendChild(document.createElement('div')).classList.add('ball');
 }
-console.log(eye_list)
-for (let i=0;i<eye_list.length;i++){
-    console.log(eye_list[i])
-    var div = document.createElement('div');
-    ball = eye_list[i].appendChild(div).classList.add('ball');
-}
-
 
 document.onmousemove = (event) => {
     const x = (event.clientX * 100) / window.innerWidth + '%';
@@ -27,4 +23,11 @@ document.onmousemove = (event) => {
         balls[i].style.top = y;
         balls[i].transform = 'translate(-' + x + ',-' + y + ')';
     }
+};
+
+const removeOne = () => {
+    document.getElementById(`eye${eye_number-1}`).remove();
+    eye_list.pop();
+    eye_number -= 1;
+    console.log(eye_list)
 };
